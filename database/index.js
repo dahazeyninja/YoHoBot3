b77b1c960b1e50063c8611d909bd857bc8cbb3d0
dahazeyninja <dahazeyniinja@gmail.com>
2020-01-28 19:40:24 -0500

    create table for renamer, cleanup

--- database/index.js
+++ database/index.js
@@ -2,21 +2,23 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('database.db');
 
 db.serialize(function(){
-	db.run('CREATE TABLE IF NOT EXISTS `rss` (`link` TEXT, PRIMARY KEY (`link`))', (err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log('[Database] RSS Link Database Ready!');
-		}
+	db.run('CREATE TABLE IF NOT EXISTS `rss` (`link` TEXT, PRIMARY KEY (`link`));', (err) => {
+		if (err) return console.error(err);
+
+		console.log('[Database] RSS Link Database Ready!');
 	});
 
-	db.run('CREATE TABLE IF NOT EXISTS `hashes` (`hash` TEXT, `match` TEXT, `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`hash`))', (err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log('[Database] Hash Database Ready!');
-		}
+	db.run('CREATE TABLE IF NOT EXISTS `hashes` (`hash` TEXT, `match` TEXT, `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`hash`));', (err) => {
+		if (err) return console.error(err);
+
+		console.log('[Database] Hash Database Ready!');
 	});
+
+	db.run('CREATE TABLE IF NOT EXISTS `transcoded` (`filename` TEXT, `path` TEXT, `match` TEXT);', (err)=>{
+		if(err) return console.error(err);
+
+		console.log('[Database] Renamer Database Ready!');
+	})
 });
 
 db.close();
